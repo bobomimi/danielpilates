de14f0e905f40fbd377b2cce95f9aef23d8d5711
ben leer <boaz242@gmail.com>
2018-02-04 12:22:50 -0600

    init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def new
+		@contact = Contact.new
+	end
+
+	def create
+		@contact = Contact.new(params[:contact])
+		@contact.request = request
+		if @contact.deliver
+			flash.now[:error] = nil
+		else
+			flash.now[:error] = 'Cannot send message.'
+			render :new
+		end
+	end
 end
--- app/controllers/freelancers_controller.rb
+++ app/controllers/freelancers_controller.rb
@@ -4,5 +4,19 @@ class FreelancersController < ApplicationController
   def index
   	@contact = Contact.new
   end
+  def new
+		@contact = Contact.new
+	end
+
+	def create
+		@contact = Contact.new(params[:contact])
+		@contact.request = request
+		if @contact.deliver
+			flash.now[:error] = nil
+		else
+			flash.now[:error] = 'Cannot send message.'
+			render :new
+		end
+	end
 end
 
